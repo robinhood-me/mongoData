904685820c3c597234a1df1e78c21cc814b8d485
Robin Hood <rhcrco@gmail.com>
2019-06-01 17:05:21 -0700

    ended request res.end(); in index.js

--- rest-api-playlist/index.js
+++ rest-api-playlist/index.js
@@ -3,6 +3,11 @@ const express = require('express');
 // setup express app
 const app = express();
 
+app.get('/', function(req, res){
+    console.log('GET request');
+    res.end();
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for requests');
